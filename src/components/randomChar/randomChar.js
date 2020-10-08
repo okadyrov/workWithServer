f65493e9095879808586d30dbf81bfafcfe8e98b
Oleg Kadyrov <abathojalyk@gmail.com>
2020-10-08 15:22:24 -0500

    reformApp

---
+++ src/components/randomChar/randomChar.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import GotService from '../../services/gotService';
+import './randomChar.css';
+import gotService from '../../services/gotService';
+export default class RandomChar extends Component {
+
+    constructor(){
+        super();
+        this.updateChar();
+    }
+
+    gotService = new GotService();
+
+    state = {
+        name: null,
+        gender: null,
+        born: null,
+        died: null,
+        culture: null
+    }
+
+    updateChar() {
+        const id = 130;
+        this.gotService.getAllCharacters(id)
+            .then((char) => {
+                this.setState({
+                    name: char.name,
+                    gender: char.gender,
+                    born: char.born,
+                    died: char.died,
+                    culture: char.culture
+                })
+            });
+    }
+    render() {
+
+        const {name, gender, born, died, culture} = this.state;
+
+        return (
+            <div className="random-block rounded">
+                <h4>Random Character: {name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Gender </span>
+                        <span>{gender}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Born </span>
+                        <span>{born}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Died </span>
+                        <span>{died}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Culture </span>
+                        <span>{culture}</span>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
